3db63582ca59c78dd94b5d576724cd8a99613a1f
sub542sah <subhamvit542@gmail.com>
2015-09-05 20:50:54 +0530

    Update resumeBuilder.js

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,8 @@
 /*
-This is empty on purpose! Your code to build the resume will go here.
+
+var awesomeThoughts="I am subham and I am AWESOME";
+console.log(awesomeThoughts);
+var funThoughts=awesomeThoughts.replace("AWESOME","FUN");
+console.log(funThoughts);
+$('#main').append(funThoughts);
  */
